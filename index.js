2874323a143d09d274aa75a304881b11f83cd8a3
Sidney Amaral <administrador@Mac-mini-de-Sidney.local>
2019-11-23 15:09:07 -0300

    ajuste

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.get('/ping', function (req, res) {
 })
 
 app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`Escutando na porta ${port}!`);
 })
